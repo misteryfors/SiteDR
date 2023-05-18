c27957f5bd1f34e70f6610b710231fa8f2ae0a71
misteryfors <47424667+misteryfors@users.noreply.github.com>
2023-05-19 00:52:32 +0300

    Add files via upload

--- shop/src/globalSetings.jsx
+++ shop/src/globalSetings.jsx
@@ -1,2 +1,2 @@
-export const baseUrl= 'http://127.0.0.1:443'
-export const baseServerUrl= 'http://127.0.0.1:442'
+export const baseUrl= 'https://master43.ru:443'
+export const baseServerUrl= 'https://master43.ru:442'
